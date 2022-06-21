5846d4e503fd355393358e8fd78b3d3d58baff6e
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2022-06-20 21:33:37 -0300

    Configurações iniciais de estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { GlobalStyles } from './styles/GlobalStyles';
 
-const App: React.FC = () => <h1>Olá, Mundo</h1>;
+const App: React.FC = () => (
+  <>
+    <h1>Control Financial</h1>
+    <GlobalStyles />
+  </>
+);
 
 export { App };
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --gray-300: #312e38;
+    --gray-500: #28262e;
+
+    --green-300: #008000;
+    --green-500: #006600;
+
+    --red-300: #c35050;
+
+    --blue-300: #005eff;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    background: #312e38;
+    color: #F4EDE8;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Fira Sans', sans-serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+export { GlobalStyles };
